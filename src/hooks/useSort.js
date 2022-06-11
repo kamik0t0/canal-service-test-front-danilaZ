b82310cea2944291b4476285a78caed51f25b9ac
kamik0t0 <Cap_NEMOx86@inbox.ru>
2022-06-11 13:58:35 +0500

    usesort refactor

--- src/hooks/useSort.js
+++ src/hooks/useSort.js
@@ -1,25 +1,30 @@
 import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { setCountriesOnPageAction } from "../redux/countries-on-page-reducer.js";
 import { sortByDistance, sortByName, sortByQuantity } from "../utils/sorts.js";
 
 export default function useSort(items) {
+    const dispatch = useDispatch();
     const [sortOrder, setSortOrder] = useState(false);
 
     const sort = (event) => {
         const sortField = event.target.innerHTML;
+        let sortedItems;
         switch (sortField) {
             case "Количество":
-                sortByQuantity(items, sortOrder);
+                sortedItems = sortByQuantity([...items], sortOrder);
                 break;
             case "Площадь кв.км":
-                sortByDistance(items, sortOrder);
+                sortedItems = sortByDistance([...items], sortOrder);
                 break;
 
             default:
-                sortByName(items, sortOrder);
+                sortedItems = sortByName([...items], sortOrder);
                 break;
         }
 
         setSortOrder(!sortOrder);
+        dispatch(setCountriesOnPageAction(sortedItems));
     };
 
     return sort;
